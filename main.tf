8f06ee3833ed95cbd81bc5dd86b47de2328204ce
sriteja28 <shreeuser2@gmail.com>
2024-09-02 08:37:12 +0530

    reorganising

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = local.region
+  region = var.region
 }
 
 provider "kubernetes" {
@@ -9,8 +9,7 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
 
@@ -22,8 +21,7 @@ provider "helm" {
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
@@ -31,76 +29,36 @@ provider "helm" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = "test-cluster"
-  region = "us-east-1"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  name   = var.name
+  region = var.region
 
   tags = {
-    GithubRepo = "github.com/sriteja28/elastic-eks-cluster"
+    Environment = var.environment
   }
 }
-
-################################################################################
-# EKS Module
-################################################################################
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
-
-  cluster_name                   = local.name
-  cluster_version                = "1.30"
-  cluster_endpoint_public_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-    cluster_addons = {
-    coredns    = {}
-    kube-proxy = {}
-    vpc-cni    = {}
-  }
-
-  eks_managed_node_groups = {
-    initial = {
-      instance_types = ["t3.medium"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-  }
-
-  tags = local.tags
-}
-
-################################################################################
-# VPC Module
-################################################################################
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  source = "../../modules/networking/vpc"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  vpc_name  = "${local.name}-vpc"
+  vpc_cidr  = var.vpc_cidr
+  azs       = data.aws_availability_zones.available.names
+  tags      = local.tags
+}
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+module "eks" {
+  source = "../../modules/compute/eks"
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
+  cluster_name  = "${local.name}-eks"
+  cluster_version = var.cluster_version
+  vpc_id        = module.vpc.vpc_id
+  subnet_ids    = module.vpc.private_subnets
+  tags          = local.tags
+}
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = local.tags
+module "ecr" {
+  source  = "../../modules/application/ecr"
+  microservices = ["service1", "service2"]
+  environment   = var.environment
+  tags          = local.tags
 }
