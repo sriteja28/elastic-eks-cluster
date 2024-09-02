ff8cdfa698f4b670491a39a52f4a1b20253a0f3e
sriteja28 <shreeuser2@gmail.com>
2024-09-02 08:43:40 +0530

    reorganising

--- ignore-practices/eks-data-pipeline/modules/application/ecr/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecr_repository_uris" {
-  description = "The URIs of the ECR repositories"
-  value       = { for ms, repo in aws_ecr_repository.microservices : ms => repo.repository_url }
-}
--- ignore-practices/eks-data-pipeline/modules/compute/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_endpoint" {
-  description = "The endpoint for the EKS cluster"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.cluster_name
-}
