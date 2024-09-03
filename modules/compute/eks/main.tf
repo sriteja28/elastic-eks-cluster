02e0d8821cc8cc35e858fe4c9c222cee0312d708
sriteja28 <shreeuser2@gmail.com>
2024-09-03 14:25:06 +0530

    update eks main.tf

--- modules/compute/eks/main.tf
+++ modules/compute/eks/main.tf
@@ -5,6 +5,7 @@ module "eks" {
   cluster_name                   = var.cluster_name
   cluster_version                = var.cluster_version
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
